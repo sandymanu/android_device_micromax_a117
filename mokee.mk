ffbeafdb4957af91a0ae089342f33f2ac2dc0ea7
Santhosh <sansans90@gmail.com>
2014-10-21 15:15:25 -0400

    a117: add support for mokee os

---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/micromax/a117/full_a117.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := Micromax A117
+PRODUCT_NAME := mk_a117
